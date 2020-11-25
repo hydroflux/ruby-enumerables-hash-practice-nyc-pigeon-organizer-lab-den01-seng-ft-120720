cab88f9c270f9f21f0dfc597e5a50f9f6dd58698
Jack Hubert <jphubert93@gmail.com>
2020-11-25 18:19:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,7 +9,7 @@ def nyc_pigeon_organizer(data)
       names.each do |name|
         # binding.pry
         if !final_array[name]
-          final_array << name
+          final_array[name] = {}
           binding.pry
         end
       end
