9ce39b6e9299177f92f1ecddc5bb47aa36e3bf86
Jack Hubert <jphubert93@gmail.com>
2020-11-25 17:46:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   # Create a list of all pigeon names
-  data[:color][:purple].select | |
+  data[:color][:purple].each_with_object do |k, v|
   
   
   
