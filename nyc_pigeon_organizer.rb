b1e8b71c89cd6ad9b9613b6ed87e1b98e861f86a
Jack Hubert <jphubert93@gmail.com>
2020-11-25 18:10:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,8 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # Create a list of all pigeon names
   data.each_with_object() do |(key, value), final_array|
-    final_array[key] = value + 3
     binding.pry
+    final_array
    end
    binding.pry
 end
